c7c89c49ef982bc268c4f6ad2dffcd70a671ad0c
Guilherme Abreu <guiii88@outlook.com>
2018-09-14 22:24:31 -0300

    reorganize main audio code

--- src/snake_main.cpp
+++ src/snake_main.cpp
@@ -8,39 +8,27 @@
 #include "../include/controler/controler.hpp"
 
 using namespace std::chrono;
-uint64_t get_now_ms() {
-  return duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
-}
+
+uint64_t get_now_ms();
+void init_asamples(std::vector<Audio::Sample*> *asamples);
+Snake *create_snake();
 
 int main ()
 {
-  Audio::Sample *asample, *asample2, *asample3, *background_asample;
-  asample = new Audio::Sample(), asample2 = new Audio::Sample(),  asample3 = new Audio::Sample();
-  background_asample = new Audio::Sample();
-  asample->load("audio/assets/blip.dat"), asample2->load("audio/assets/pao.dat");
-  asample3->load("audio/assets/naovaidar.dat"), background_asample->load("audio/assets/background.dat");
+  std::vector<Audio::Sample* > asamples(13);
+  init_asamples(&asamples);
 
-  Audio::Player *player, *player2, *player3, *background_player;
-  player = new Audio::Player(), player2 = new Audio::Player(), player3 = new Audio::Player();
-  background_player = new Audio::Player();
-  player->init(44100, 64, 0.6), player2->init(38100, 256, 1.5), player3->init(44100, 256, 2.5), background_player->init(44100, 2048, 0.2);
-  player->play(asample), player2->play(asample2), background_player->play(background_asample);
+  Audio::Player *button_player, *soundboard_player, *background_player;
+  button_player = new Audio::Player(), soundboard_player = new Audio::Player(), background_player = new Audio::Player();
+  button_player->init(44100, 64, 0.6), soundboard_player->init(44100, 256, 2.5), background_player->init(44100, 2048, 0.2);
 
-  /*
-  std::thread asmpl_thread;
-  std::thread newthread(player_thread, background_asample);
-  asmpl_thread.swap(newthread);*/
+  asamples[0]->set_position(1000000);
+  button_player->play(asamples[0]);
 
-  pos_2d p = {40,40};
-  vel_2d v = {(float)VEL,0};
+  background_player->play(asamples[3]);
 
-  Snake *snake = new Snake();
-  for (int i =0; i < 8; i++){
-    Corpo *c = new Corpo(v, p);
-    snake->add_corpo(c);
-    p.x-=1;
-  }
-
+  Snake *snake = create_snake();
+  
   ListaDeSnakes *l = new ListaDeSnakes();
   l->add_snake(snake);
   Fisica *f = new Fisica(l);
@@ -66,16 +54,17 @@ int main ()
 
     if (f->food_pos.x == -1 && f->food_pos.y == -1){
       f->feed_snake();
-      asample2->set_position(0);
+      soundboard_player->play(asamples[1]);
+      asamples[1]->set_position(0);
     }
 
-    if (background_asample->finished())
-      background_asample->set_position(0);
-      
+    if (asamples[3]->finished())
+      asamples[3]->set_position(0);
+
     // Atualiza modelo
     if(f->update(deltaT) && deltaT!=0) {
       background_player->stop();
-      player3->play(asample3);
+      soundboard_player->play(asamples[2]);
       std::this_thread::sleep_for (std::chrono::milliseconds(3000));
       clear();
       move((int)LINES/2, (int)COLS/2);
@@ -95,19 +84,19 @@ int main ()
     switch (c){
       case 's':
         f->change_dir(0,0);
-        asample->set_position(0);
+        asamples[0]->set_position(0);
         break;
       case 'a':
         f->change_dir(1,0);
-        asample->set_position(0);
+        asamples[0]->set_position(0);
         break;
       case 'w':
         f->change_dir(2,0);
-        asample->set_position(0);
+        asamples[0]->set_position(0);
         break;
       case 'd':
         f->change_dir(3,0);
-        asample->set_position(0);
+        asamples[0]->set_position(0);
         break;
     }
     
@@ -127,10 +116,48 @@ int main ()
   }
 
   //asmpl_thread.join();
-  player3->stop();
-  player2->stop();
-  player->stop();
+  button_player->stop();
+  soundboard_player->stop();
   tela->stop();
   teclado->stop();
   return 0;
+}
+
+uint64_t get_now_ms() {
+  return duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
+}
+
+Snake *create_snake(){
+  pos_2d p = {40,40};
+  vel_2d v = {(float)VEL,0};
+
+  Snake *snake = new Snake();
+  for (int i =0; i < 8; i++){
+    Corpo *c = new Corpo(v, p);
+    snake->add_corpo(c);
+    p.x-=1;
+  }
+
+  return snake;
+}
+
+void init_asamples(std::vector<Audio::Sample*> *asamples){
+  for (int i = 0; i < asamples->size(); i++){
+    (*asamples)[i] = new Audio::Sample();
+  }
+
+  (*asamples)[0]->load("audio/assets/blip.dat");
+  (*asamples)[1]->load("audio/assets/pao.dat");
+  (*asamples)[2]->load("audio/assets/naovaidar.dat");
+  (*asamples)[3]->load("audio/assets/background.dat");
+  (*asamples)[4]->load("audio/assets/get_over_here.dat");
+  (*asamples)[5]->load("audio/assets/come_here.dat");
+  (*asamples)[6]->load("audio/assets/mortal_kombat_theme.dat");
+  (*asamples)[7]->load("audio/assets/finish_him.dat");
+  (*asamples)[8]->load("audio/assets/finish_her.dat");
+  (*asamples)[9]->load("audio/assets/fatality.dat");
+  (*asamples)[10]->load("audio/assets/animality.dat");
+  (*asamples)[11]->load("audio/assets/soul_suffer.dat");
+  (*asamples)[12]->load("audio/assets/zeruela.dat");
+  return;
 }
